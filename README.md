9415a4e00e9a9a71eba1c8623f18c4a3a7e6cbce
Daoyuan Li <daoyuan.li@weconnexion.com>
2015-03-29 21:34:11 +0200

    Package hallo.js.

---
+++ README.md
@@ -0,0 +1,13 @@
+Hallo
+============
+
+[Hallo](https://github.com/bergie/hallo) is the simplest web editor imaginable.
+Now available in meteor package.
+
+# Install
+```sh
+meteor add daoli:hallo
+```
+
+# Use
+TODO
---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'daoli:hallo',
+  version: '1.1.1',
+  summary: 'Hallo.js (the simplest web editor imaginable) for Meteor',
+  git: 'https://github.com/daoli/hallo.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.5');
+
+  api.use(['jquery', 'mrt:rangy', 'mizzao:jquery-ui']);
+  api.imply(['mrt:rangy', 'mizzao:jquery-ui']);
+
+  api.addFiles('dist/hallo.js');
+});
+
+#Package.onTest(function(api) {
+#  api.use('tinytest');
+#  api.use('daoli:hallo');
+#  api.addFiles('hallo-tests.js');
+#});
