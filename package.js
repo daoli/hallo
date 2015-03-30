21ef4b4cd637096db3879f99b7a34a238f20d415
Daoyuan Li <daoyuan.li@weconnexion.com>
2015-03-30 20:32:28 +0200

    Put rangy inside this package to remove JS errors on Chrome

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'daoli:hallo',
-  version: '1.1.1_2',
+  version: '1.1.1_3',
   summary: 'Hallo.js (the simplest web editor imaginable) for Meteor',
   git: 'https://github.com/daoli/hallo.git',
   documentation: 'README.md'
@@ -9,9 +9,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.5');
 
-  api.use(['jquery'], 'client');
-  api.use(['mrt:rangy@0.0.1', 'mrt:jquery-ui@1.9.2']);
-  api.imply(['mrt:rangy', 'mrt:jquery-ui']);
+  api.use(['jquery', 'mrt:jquery-ui@1.9.2'], 'client');
 
-  api.addFiles('dist/hallo.js', 'client');
+  api.addFiles('lib/rangy/rangy-core.js', 'client');
+  api.addFiles('lib/hallo/hallo.js', 'client');
 });
